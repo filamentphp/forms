f73f80d15de8024fb51a24fc33f8ccc798863a23
Dan <dan@danharrin.com>
2021-04-12 22:04:28 +0000

    Merge pull request #344 from tanthammar/patch-1

---
+++ resources/lang/sv/fields.php
@@ -0,0 +1,139 @@
+<?php
+
+return [
+
+    'dateTimePicker' => [
+
+        'abbreviatedDays' => [
+            'friday' => 'Fre',
+            'monday' => 'Mån',
+            'saturday' => 'Lör',
+            'sunday' => 'Sön',
+            'thursday' => 'Tor',
+            'tuesday' => 'Tis',
+            'wednesday' => 'Ons',
+        ],
+
+        'months' => [
+            'april' => 'April',
+            'august' => 'Augusti',
+            'december' => 'December',
+            'february' => 'Februari',
+            'january' => 'Januari',
+            'july' => 'Juli',
+            'june' => 'Juni',
+            'march' => 'Mars',
+            'may' => 'Maj',
+            'november' => 'November',
+            'october' => 'Oktober',
+            'september' => 'September',
+        ],
+
+    ],
+
+    'keyValue' => [
+
+        'buttons' => [
+
+            'add' => [
+                'label' => 'Ny Rad',
+            ],
+
+            'delete' => [
+                'label' => 'Radera Rad',
+            ],
+
+            'sort' => [
+                'label' => 'Tryck, ordna',
+            ],
+
+        ],
+
+        'fields' => [
+
+            'key' => [
+                'label' => 'Namn',
+                'placeholder' => 'Ange namn...',
+            ],
+
+            'value' => [
+                'label' => 'Värde',
+                'placeholder' => 'Ange ett värde...',
+            ],
+
+        ],
+
+    ],
+
+    'markdownEditor' => [
+
+        'toolbarButtons' => [
+            'attachFiles' => 'Lägg till filer',
+            'bold' => 'Fet',
+            'bullet' => 'Lista',
+            'code' => 'Kod',
+            'italic' => 'Kursiv',
+            'link' => 'Länk',
+            'number' => 'Nummer',
+            'preview' => 'Förhandsvisa',
+            'strike' => 'Överstruken',
+            'write' => 'Skriv',
+        ],
+
+    ],
+
+    'multiSelect' => [
+        'emptyOptionsMessage' => 'Finns inga alternativ.',
+        'noSearchResultsMessage' => 'Hittade inget som matchade din sökning.',
+        'placeholder' => 'Välj ett alternativ',
+    ],
+
+    'richEditor' => [
+
+        'dialogs' => [
+
+            'link' => [
+
+                'buttons' => [
+                    'link' => 'Länka',
+                    'unlink' => 'Radera länk',
+                ],
+
+                'label' => 'URL',
+
+                'placeholder' => 'Ange en URL',
+
+            ],
+
+        ],
+
+        'toolbarButtons' => [
+            'attachFiles' => 'Lägg till filer',
+            'bold' => 'Fet',
+            'bullet' => 'Lista',
+            'code' => 'Kod',
+            'heading' => 'Rubrik',
+            'italic' => 'Kursiv',
+            'link' => 'Länk',
+            'number' => 'Nummer',
+            'quote' => 'Citat',
+            'redo' => 'Igen',
+            'strike' => 'Överstruken',
+            'subheading' => 'Underrubrik',
+            'title' => 'Titel',
+            'undo' => 'Ångra',
+        ],
+
+    ],
+
+    'select' => [
+        'emptyOptionsMessage' => 'Börja skriva för att söka...',
+        'noSearchResultsMessage' => 'Hittar inget som matchar din sökning.',
+        'placeholder' => 'Välj ett alternativ',
+    ],
+
+    'tags' => [
+        'placeholder' => 'Ny tagg',
+    ],
+
+];
