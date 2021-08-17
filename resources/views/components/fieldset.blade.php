b95f0b6c327ff5dce09a7e681cd108ad06fc2a3f
Dan Harrin <git@danharrin.com>
2021-08-17 11:30:33 +0100

    Update fieldset.blade.php

--- resources/views/components/fieldset.blade.php
+++ resources/views/components/fieldset.blade.php
@@ -1,5 +1,5 @@
 <fieldset class="rounded-xl shadow-sm border border-gray-300 p-6">
-    <legend class="text-sm leading-tight font-medium px-2">
+    <legend class="text-sm leading-tight font-medium px-2 -ml-2">
         {{ $getLabel() }}
     </legend>
 
