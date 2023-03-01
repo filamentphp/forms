3ab15f1b6382d2fe7617a66cdc28e10dfeac7202
Dan <git@danharrin.com>
2023-03-01 22:54:45 +0000

    Recompile

--- resources/views/components/checkbox-list.blade.php
+++ resources/views/components/checkbox-list.blade.php
@@ -20,13 +20,12 @@
         visibleCheckboxListOptions: [],
 
         init: function () {
+            this.updateVisibleCheckboxListOptions()
             this.checkIfAllCheckboxesAreChecked()
 
             Livewire.hook('message.processed', () => {
                 this.checkIfAllCheckboxesAreChecked()
             })
-
-            this.updateVisibleCheckboxListOptions()
 
             $watch('search', () => {
                 this.updateVisibleCheckboxListOptions()
