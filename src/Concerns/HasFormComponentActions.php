6e4e73a52bd5dec2606664ae5a7194dd968d3856
Dan <git@danharrin.com>
2023-06-21 09:45:19 +0000

    Card subheading -> description

--- src/Concerns/HasFormComponentActions.php
+++ src/Concerns/HasFormComponentActions.php
@@ -169,9 +169,9 @@ trait HasFormComponentActions
             return false;
         }
 
-        return $action->getModalSubheading() ||
+        return $action->getModalDescription() ||
             $action->getModalContent() ||
-            $action->getModalFooter() ||
+            $action->getModalContentFooter() ||
             $action->getInfolist() ||
             $this->mountedFormComponentActionHasForm();
     }
