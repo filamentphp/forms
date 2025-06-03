d73cdda057a4f5bd409eab9573101e73edb404cc
Dan <git@danharrin.com>
2025-06-03 13:40:37 +0000

    Update Select.php

--- src/Components/Select.php
+++ src/Components/Select.php
@@ -782,7 +782,7 @@ class Select extends Field implements Contracts\CanDisableOptions, Contracts\Has
 
             if (isset($baseRelationshipQuery->limit)) {
                 $component->optionsLimit($baseRelationshipQuery->limit);
-            } elseif ($component->isSearchable()) {
+            } elseif ($component->isSearchable() && filled($component->getSearchColumns())) {
                 $relationshipQuery->limit($component->getOptionsLimit());
             }
 
@@ -1089,7 +1089,7 @@ class Select extends Field implements Contracts\CanDisableOptions, Contracts\Has
         $query->where(function (Builder $query) use ($databaseConnection, $isForcedCaseInsensitive, $search): Builder {
             $isFirst = true;
 
-            foreach ($this->getSearchColumns() as $searchColumn) {
+            foreach ($this->getSearchColumns() ?? [] as $searchColumn) {
                 $whereClause = $isFirst ? 'where' : 'orWhere';
 
                 $query->{$whereClause}(
@@ -1215,6 +1215,10 @@ class Select extends Field implements Contracts\CanDisableOptions, Contracts\Has
 
     public function hasDynamicSearchResults(): bool
     {
+        if ($this->hasRelationship() && blank($this->getSearchColumns())) {
+            return false;
+        }
+
         return $this->getSearchResultsUsing instanceof Closure;
     }
 
